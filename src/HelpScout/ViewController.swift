e4ada8f633c18aa7d178473e44e528e6b3f87c9d
Frank Mendoza <desarrollo@globalstd.com>
2016-12-09 10:20:16 -0600

    First commit

---
+++ src/HelpScout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelpScout
+//
+//  Created by Software GlobalSTD on 09/12/16.
+//  Copyright © 2016 GlobalSTD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
